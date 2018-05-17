cd2a9f5e70a6823bcdfc8a14420ccba1cadd1824
rebadiali <renata.badiali@gmail.com>
2018-05-16 21:03:35 -0300

    Arrumar as imagens dos hexágonos

--- src/Components/Comunidade/Comunidade.js
+++ src/Components/Comunidade/Comunidade.js
@@ -3,13 +3,13 @@ import './Comunidade.css';
 import Hexagono from '../Hexagono/Hexagono';
 import com1 from '../../Images/dj.png';
 import com2 from '../../Images/_net.png';
-import com9 from '../../Images/desprograme.png';
+import com3 from '../../Images/programaria.jpg';
 import com4 from '../../Images/railgirls.jpg';
 import com5 from '../../Images/pyladies.png';
 import com6 from '../../Images/reprograma.jpg';
 import com7 from '../../Images/wordpress.jpeg';
 import com8 from '../../Images/phpwomen.png';
-import com3 from '../../Images/programaria.jpg';
+import com9 from '../../Images/desprograme.png';
 import com10 from '../../Images/jssp.png';
 
 class Comunidade extends Component {
@@ -20,20 +20,20 @@ class Comunidade extends Component {
         <hr /> <hr />
         <div id="colmeia">
             <div className="comunity1">
-                <Hexagono imagem={com1} />
-                <Hexagono imagem={com2} />
-                <Hexagono imagem={com3} />
+                <Hexagono imagem={com1} color={'#F7941E'} />
+                <Hexagono imagem={com2} color={'#F2F2F2'} />
+                <Hexagono imagem={com3} color={'#223544'} />
             </div>
             <div className="comunity2">
-                <Hexagono imagem={com4} />
-                <Hexagono imagem={com5} />
-                <Hexagono imagem={com6} />
-                <Hexagono imagem={com7} />
+                <Hexagono imagem={com4} color={'#680102'} />
+                <Hexagono imagem={com5} color={'#ED2B4D'} />
+                <Hexagono imagem={com6} color={'#422F4B'} />
+                <Hexagono imagem={com7} color={'#000000'} />
             </div>
             <div className="comunity1">
-                <Hexagono imagem={com8} />
-                <Hexagono imagem={com9} />
-                <Hexagono imagem={com10} />
+                <Hexagono imagem={com8} color={'#41203F'} />
+                <Hexagono imagem={com9} color={'#CE595A'} />
+                <Hexagono imagem={com10} color={'#F7DF1E'} />
             </div>
         </div>
     </section>
--- src/Components/Hexagono/Hexagono.js
+++ src/Components/Hexagono/Hexagono.js
@@ -6,22 +6,21 @@ class Hexagono extends Component{
     constructor(props){
         super(props);
         this.background = "url(" + this.props.imagem + ")";
+        this.color = this.props.color;
     }    
 
     render = function () {
         console.log(this.background);
         return (
+            <div>
+                <div className="hexagon" style={{background: this.background}}>
+                <div className="hexTop" style={{background: this.color}}></div>
+                <div className="hexBottom" style={{background: this.color}}></div>
+                </div>
             
-       <div>
-            <div className="hexagon" style={{background: this.background}}>
-              <div className="hexTop"></div>
-              <div className="hexBottom"></div>
             </div>
-         
-        </div>
-    );
+        );
     }
- 
 }
 
 export default Hexagono;
